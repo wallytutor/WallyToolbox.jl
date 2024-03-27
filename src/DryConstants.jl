969e692bedc2831d871cec9c3063544dc13afa92
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2024-03-27 16:49:02 +0100

    Added constants

---
+++ src/DryConstants.jl
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+module DryConstants
+
+"Ideal gas constant [J/(mol.K)]."
+const GAS_CONSTANT::Float64 = 8.314_462_618_153_24
+
+"Stefan-Boltzmann constant [W/(m².K⁴)]."
+const STEFAN_BOLTZMANN::Float64 = 5.670_374_419e-08
+
+"Reference atmospheric pressure [Pa]."
+const PREF::Float64 = 101325.0
+
+"Normal atmospheric temperature [K]."
+const TREF::Float64 = 273.15
+
+end # (module DryConstants)
