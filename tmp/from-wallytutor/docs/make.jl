f11f4e4481fb1a4c0e3bb469aa71a70d6eae9bed
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2024-03-30 16:21:05 +0100

    Stripping

--- tmp/from-wallytutor/docs/make.jl
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-include("common.jl")
-
-deploydocs(;
-    repo="github.com/wallytutor/DryTooling.jl",
-    devbranch="main",
-)
--- tmp/from-wallytutor/src/FiniteVolumes/tests.jl
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-using DryTooling
-using DryTooling.FiniteVolumes
