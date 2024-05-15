2a2e830821b84de7e0f7183974d36afdfaac4424
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2024-05-15 17:56:21 +0200

    Getting things right

--- apps/Elmer/steady_heat/variable/refractory.f90
+++ apps/Elmer/steady_heat/variable/refractory.f90
@@ -9,18 +9,18 @@ FUNCTION conductivity(model, n, time) RESULT(k)
     REAL(KIND=dp) :: time, k
 
     ! Function internals.
-    TYPE(Variable_t), POINTER :: TemperatureVariable
+    TYPE(Variable_t), POINTER :: varptr
     REAL(KIND=dp) :: T
     INTEGER :: idx
 
     ! Retrieve pointer to the temperature variable.
-    TemperatureVariable => VariableGet(model%Variables, 'Temperature')
+    varptr => VariableGet(model%Variables, 'Temperature')
 
     ! Access index of current node.
-    idx = TemperatureVariable%Perm(n)
+    idx = varptr%Perm(n)
 
     ! Retrieve nodal temperature.
-    T = TemperatureVariable%Values(idx)
+    T = varptr%Values(idx)
 
     ! Compute heat conductivity from NodalTemperature, k=k(T)
     k = 1.984706119175 - T * (2.531209e-03 - 1.43e-06 * T)
--- apps/Elmer/steady_heat/variable/runall.ps1
+++
@@ -1,6 +0,0 @@
-# Parse and exit to generate mesh.
-gmsh - geometry.geo
-
-# Compile extension with material properties.
-elmerf90 -o refractory.dll refractory.f90
-
