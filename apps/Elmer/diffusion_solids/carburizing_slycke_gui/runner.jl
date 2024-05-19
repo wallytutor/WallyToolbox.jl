835ef61df4da0da057981738925bf56046a0eeb5
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2024-05-19 15:32:42 +0200

    Finishing

--- apps/Elmer/diffusion_solids/carburizing_slycke_gui/runner.jl
+++ apps/Elmer/diffusion_solids/carburizing_slycke_gui/runner.jl
@@ -2,22 +2,40 @@
 using WallyToolbox
 using DryUtilities: redirect_to_files
 
+function cleanup()
+    opts = (force = true, recursive = true)
+
+    isfile("ELMERSOLVER_STARTINFO") && rm("ELMERSOLVER_STARTINFO")
+
+    rm("partitioning.$(NP)/"; opts...)
+    rm("sample/"; opts...)
+    rm("logs/"; opts...)
+    rm("results/"; opts...)
+    
+    return nothing
+end
+
+cleanup()
+
 NP = 4
 MPI = "mpiexec"
 
+@info "Enforcing logging directory..."
+!isdir("logs") && mkdir("logs")
+
 @info "Generating mesh..."
-redirect_to_files("log.gmsh") do
+redirect_to_files("logs/log.gmsh") do
     run(`gmsh -2 sample.geo -order 2 -format msh2`)
 end
 
 @info "Converting mesh to Elmer grid format..."
-redirect_to_files("log.grid") do
+redirect_to_files("logs/log.grid") do
     run(`ElmerGrid 14 2 sample.msh -autoclean -merge 1.0e-05`)
     rm("sample.msh"; force = true)
 end
 
 @info "Solving/integrating problem (may take a while)..."
-redirect_to_files("log.solver") do
+redirect_to_files("logs/log.solver") do
     open("ELMERSOLVER_STARTINFO", "w") do fp
         write(fp, "case.sif\n1\n")
     end
@@ -31,10 +49,5 @@ redirect_to_files("log.solver") do
 
     rm("ELMERSOLVER_STARTINFO"; force = true)
 end
-
-@info "Cleaning up logs..."
-!isdir("logs") && mkdir("logs")
-moov(f) = mv(f, "logs/$(f)"; force = true)
-map(moov, filter(startswith("log."), readdir()))
 
 @info "DONE!"
