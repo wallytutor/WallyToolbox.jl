cc6c39d1e78a69ade8c5e35bcde92c02ef718c18
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2024-05-22 07:10:44 +0200

    Work in progress

--- apps/Elmer/radiation/particles_grid_regular/packing.geo
+++ apps/Elmer/radiation/particles_grid_regular/packing.geo
@@ -32,12 +32,43 @@ s = news;
 Rectangle(s) = {0.0, 0.0, 0, Lx, Ly, 0};
 
 For particle In {1:Nx*Ny}
-    BooleanIntersection{ Surface{s}; }{ Surface{particle}; Delete; }
+    inter = BooleanIntersection{ Surface{s}; }{ Surface{particle}; Delete; };
+    // Printf("%g", inter);
 EndFor
 
 n = 1;
-For s In {2:Nx*Ny}
-    n = BooleanUnion{ Surface{n}; Delete; }{ Surface{s}; Delete; };
+For particle In {2:Nx*Ny}
+    n = BooleanUnion{ Surface{n}; Delete; }{ Surface{particle}; Delete; };
 EndFor
 
-BooleanIntersection{ Surface{1}; Delete; }{ Surface{s}; Delete; };
+final = BooleanIntersection{ Surface{1}; Delete; }{ Surface{31}; Delete; };
+
+Transfinite Curve {1, 3} = 10*Ny*D;
+Transfinite Curve {2, 4} = 10*Nx*D;
+// Recombine Surface {final};
+
+Physical Surface(1) = {final};
+
+Physical Curve(1001) = {1};
+Physical Curve(1002) = {3};
+Physical Curve(1003) = {2, 4};
+
+// TODO find a reliable way to automate this!
+Physical Curve(1004) = {
+     5, 6, 7, 8, 9, 10,
+    11, 12, 13, 14, 15,
+    16, 17, 18, 19, 20,
+    21, 22, 23, 24, 25,
+    26, 27, 28, 29, 30,
+    31, 32, 33, 34, 35,
+    36, 37, 38, 39, 40,
+    41, 42, 43, 44, 45,
+    46, 47, 48, 49, 50,
+    51, 52, 53, 54, 55,
+    56, 57, 58, 59, 60,
+    61, 62, 63, 64, 65,
+    66, 67, 68, 69, 70,
+    71, 72, 73, 74, 75,
+    76, 77, 78, 79, 80,
+    81, 82, 83, 84
+};
---
+++ apps/Elmer/radiation/particles_grid_regular/runner.jl
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+using WallyToolbox
+using DryUtilities: redirect_to_files
+
+function cleanup(np)
+    opts = (force = true, recursive = true)
+
+    isfile("ELMERSOLVER_STARTINFO") && rm("ELMERSOLVER_STARTINFO")
+
+    rm("logs/"; opts...)
+    rm("results/"; opts...)
+    # rm("partitioning.$(np)/"; opts...)
+    # rm("sample/"; opts...)
+    
+    return nothing
+end
+
+function workflow(;
+        geom       = "packing.geo",
+        np         = 4, 
+        mpi        = "mpiexec", 
+        cleanstart = true
+    )
+    cleanstart && cleanup(np)
+
+    @info "Enforcing logging directory..."
+    !isdir("logs") && mkdir("logs")
+    
+    @info "Generating mesh..."
+    redirect_to_files("logs/log.gmsh") do
+        run(`gmsh -2 $(geom) -order 2 -format msh2`)
+    end
+
+    # @info "Converting mesh to Elmer grid format..."
+    # redirect_to_files("logs/log.grid") do
+    #     run(`ElmerGrid 14 2 sample.msh -autoclean -merge 1.0e-05`)
+    #     rm("sample.msh"; force = true)
+    # end
+
+    # @info "Solving/integrating problem (may take a while)..."
+    # redirect_to_files("logs/log.solver") do
+    #     open("ELMERSOLVER_STARTINFO", "w") do fp
+    #         write(fp, "case.sif\n1\n")
+    #     end
+        
+    #     if (NP > 0)
+    #         run(`ElmerGrid 2 2 sample -partdual -metiskway $(NP)`)
+    #         run(`$(MPI) -n $(NP) ElmerSolver_mpi`)
+    #     else
+    #         run(`ElmerSolver`)
+    #     end
+
+    #     rm("ELMERSOLVER_STARTINFO"; force = true)
+    # end
+
+    @info "DONE!"
+end
+
+workflow()
+
+
+
+
