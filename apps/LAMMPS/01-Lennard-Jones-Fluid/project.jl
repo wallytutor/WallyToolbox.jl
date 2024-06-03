186050bfa52cfd01c9912976c565bd51391946ff
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2024-06-03 20:51:46 +0200

    Splitting directories

--- apps/LAMMPS/01-Lennard-Jones-Fluid/project.jl
+++ apps/LAMMPS/01-Lennard-Jones-Fluid/project.jl
@@ -1,4 +1,40 @@
 # -*- coding: utf-8 -*-
-run(`lmp -in inputs.1a.lammps`)
-run(`lmp -in inputs.2a.lammps`)
-run(`lmp -in inputs.2b.lammps`)
+using WallyToolbox
+using DryUtilities: redirect_to_files
+
+function run_tutorial(; script, wd, force = false)
+    !isfile(joinpath(wd, script)) && throw("No $(script) in $(wd)!")
+
+    logname = joinpath(wd, "log.$(basename(script))")
+
+    if isfile(logname) && !force
+        @warn("Already run $(script) (see $(logname))")
+        return 99
+    end
+
+    @info("Running $(script) at $(wd) (see $(logname))")
+
+    return redirect_to_files(logname) do
+        run(Cmd(`lmp -in $(script)`, dir=wd))
+    end
+end
+
+force = true
+
+project = [
+    (script = "inputs-1.lammps",  wd = "tutorial-1"),
+    (script = "inputs-1.lammps",  wd = "tutorial-2"),
+    (script = "inputs-2.lammps",  wd = "tutorial-2"),
+    (script = "inputs-1.lammps",  wd = "exercise-1"),
+    (script = "reference.lammps", wd = "exercise-1"),
+    (script = "inputs-1.lammps",  wd = "exercise-2"),
+    (script = "inputs-2.lammps",  wd = "exercise-2"),
+    (script = "inputs-3.lammps",  wd = "exercise-2"),
+    (script = "reference.lammps", wd = "exercise-2"),
+    # (script = "inputs-1.lammps",  wd = "exercise-3"),
+    # (script = "reference.lammps", wd = "exercise-3"),
+]
+
+for simulation in project
+    run_tutorial(; simulation...)
+end
