8b87c96c1aa7d74661f9bbc7fb644f914d36d01a
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2024-07-04 13:36:38 +0200

    Homogeneous deps

--- src/WallyDocuments.jl
+++ src/WallyDocuments.jl
@@ -4,6 +4,9 @@ module WallyDocuments
 using Documenter
 using Documenter.DocMeta: setdocmeta!
 using Documenter.HTMLWriter: relhref
+using JuliaFormatter
+using Pluto: ServerSession, SessionActions
+using Pluto: generate_html
 
 export get_format, deployrepo, julianizemarkdown
 export formatnotecells, formatequations
@@ -122,6 +125,31 @@ function formatequations(text)
     oldgroup = Regex(join([ODOLLAR, ANYNAME, CDOLLAR]))
     DEBUGMATCHES && matchdebugger(oldgroup, text)
     return replace(text, oldgroup => SDOLLAR)
+end
+
+"Convert Pluto notebook into a static HTML file."
+function convert_pluto(nblist::Vector{String}; force=false, verbose=2)
+    s = ServerSession()
+    s.options.server.launch_browser = false
+
+    for nbname in nblist
+        nbpath = joinpath(@__DIR__, "$(nbname).jl")
+        pgpath = joinpath(@__DIR__, "$(nbname).html")
+        
+        if !format_file(nbpath)
+            @warn "file not formatted: $(nbpath)"
+        end
+        
+        if isfile(pgpath) && !force
+            verbose > 1 && @info "file exists: $(pgpath)"
+            return
+        end
+        
+        verbose > 0 && @info "working on $(nbname)"
+        nb = SessionActions.open(s, nbpath; run_async=false)
+        write(pgpath, generate_html(nb))
+        SessionActions.shutdown(s, nb)
+    end
 end
 
 end # (module WallyDocuments)
