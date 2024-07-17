7a21be64005d5aaa494210625af9f7b974a42100
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2024-07-17 10:21:44 +0200

    Organized tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,18 +1,5 @@
 # -*- coding: utf-8 -*-
+using WallyToolbox
 using Test
 
-@testset "DryTransport" begin
-
-    nu_gn = NusseltGnielinski()
-    nu_db = NusseltDittusBoelter()
-
-    aspect_ratio = 100.0
-    validate = true
-
-    # try nusselt(nu_gn, 5.0e+07, 0.7; validate)                     catch end
-    # try nusselt(nu_gn, 5.0e+03, 0.4; validate)                     catch end
-    # try nusselt(nu_gn, 5.0e+07, 0.4; validate)                     catch end
-    # try nusselt(nu_db, 5.0e+03, 0.7; validate, aspect_ratio)       catch end
-    # try nusselt(nu_db, 5.0e+04, 0.5; validate, aspect_ratio)       catch end
-    # try nusselt(nu_db, 5.0e+04, 0.7; validate, aspect_ratio = 1.0) catch end
-end
+include("test-DryTransport.jl")
