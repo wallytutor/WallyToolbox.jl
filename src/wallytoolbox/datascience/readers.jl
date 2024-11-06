1a767a9e4427160d1889b3a2840ca32280389bbe
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2024-11-06 14:53:22 +0100

    Added some data and parser

--- src/wallytoolbox/datascience/readers.jl
+++ src/wallytoolbox/datascience/readers.jl
@@ -3,6 +3,7 @@
 ##############################################################################
 
 export read_csv
+export read_janaf
 
 ##############################################################################
 # CSV
@@ -14,7 +15,54 @@ function read_csv(fname;
         comment_char = '#'
     )::DataFrame
 	data, header = readdlm(fname, sep; header=true, comments, comment_char)
-	return DataFrame(data, vec(Symbol.(header)))
+	return dlm2df(data, header)
+end
+
+##############################################################################
+# JANAF Tables (not Data Science but why not!?)
+##############################################################################
+
+function read_janaf(fname; verbose=true)
+    verbose && @warn("""\
+        This function is fragile, please verify the downloaded JANAF table \
+        you are trying to read is well formed; a common issue when copying \
+        data is the presence of some spaces where tabs should be.
+    """)
+	opts = (skipstart=2, comments=true, header=true)
+	data, header = readdlm(fname, '\t'; opts...)
+	return dlm2df(parse_matrix_with_nan(data), header)
+end
+
+##############################################################################
+# Internals
+##############################################################################
+
+# This snippet happens all the time when using DelimitedFiles/DataFrames.
+dlm2df(data, header) = DataFrame(data, vec(Symbol.(header)))
+
+
+function try_parse_number(v, target)
+    # It is already a number? Got it!
+	isa(v, Number) && return v
+
+    # Try only once here:
+	parsed = tryparse(target, v)
+	
+    # It is not a number, then NaN-it!
+	isnothing(parsed) && return NaN
+
+    # It's a number!
+	return parsed
+end
+
+
+function parse_vector_with_nan(data; target = Float64)
+	return [try_parse_number(x, target) for x in data]
+end
+
+
+function parse_matrix_with_nan(data)
+	return hcat([parse_vector_with_nan(row) for row in eachcol(data)]...)
 end
 
 ##############################################################################
