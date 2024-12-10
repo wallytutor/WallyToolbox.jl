1196b650a3176c9eddeee667833ae053cbdfba3f
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2024-12-10 14:40:58 +0100

    Merging drafts

--- docs/src/Notebooks/Pluto/cantera-for-combustion.jl
+++ docs/src/Notebooks/Pluto/cantera-for-combustion.jl
@@ -717,7 +717,331 @@ md"""
 """
 
 # ╔═╡ 37c27593-b099-417f-83c4-517eeb670aba
+md"""
+In this notebook we adapt this [reference from Cantera](https://cantera.org/examples/jupyter/thermo/flame_temperature.ipynb.html) to compute flame temperature produced by mixing hydrogen and air. This initial estimations are used to check the suitability from current models and kinetics mechanisms to applications to high temperature material processing. Overall combustion equation at unity fuel to oxidizer ratio is
 
+```math
+\mathrm{H_2 + 1/2 O_2 + 1.88 N_2 \rightarrow H_2O + 1.88 N_2 + \Delta{H}}
+```
+
+For complete combustion a filtered version of the mechanism is used so that nominal released heat is found.
+
+A second solution object is created with the whole set of species to compute incomplete combustion.
+
+```python
+species_keep = ("H2", "O2", "H2O", "N2")
+species_full = {s.name: s for s in ct.Species.listFromFile("h2o2.yaml")}
+species_calc = [species_full[s] for s in species_keep]
+
+gas1 = ct.Solution(thermo="IdealGas", species=species_calc)
+gas2 = ct.Solution(thermo="IdealGas", species=species_full.values())
+
+TP = 298.15, ct.one_atm
+
+X_fuel = "H2:1.0"
+X_oxid = "O2:1.0, N2:3.76"
+
+phi = np.linspace(0.5, 2.5, 1000)
+
+T_complete = np.zeros_like(phi)
+T_incomplete = np.zeros_like(phi)
+
+X_complete = np.zeros_like(phi)
+X_incomplete = np.zeros_like(phi)
+
+lhv_complete = np.zeros_like(phi)
+lhv_incomplete = np.zeros_like(phi)
+
+for k, phik in enumerate(phi):
+    gas1.TP = TP
+    gas2.TP = TP
+
+    gas1.set_equivalence_ratio(phik, X_fuel, X_oxid, basis="mole")
+    gas2.set_equivalence_ratio(phik, X_fuel, X_oxid, basis="mole")
+
+    h0_1 = gas1.enthalpy_mass
+    h0_2 = gas2.enthalpy_mass
+    
+    y_fuel1 = gas1["H2"].Y[0]
+    y_fuel2 = gas2["H2"].Y[0]
+    
+    gas1.equilibrate("HP", solver="vcs", max_steps=10_000, max_iter=1000, log_level=0)
+    gas2.equilibrate("HP", solver="vcs", max_steps=10_000, max_iter=1000, log_level=0)
+
+    T_complete[k] = gas1.T
+    T_incomplete[k] = gas2.T
+
+    X_complete[k] = gas1.mole_fraction_dict()["O2"]
+    X_incomplete[k] = gas2.mole_fraction_dict()["O2"]
+    
+    gas1.TP = TP
+    gas2.TP = TP
+    
+    h1_1 = gas1.enthalpy_mass
+    h1_2 = gas2.enthalpy_mass
+    
+    lhv_complete[k] = -(h1_1 - h0_1) / (y_fuel1 * 1e6)
+    lhv_incomplete[k] = -(h1_2 - h0_2) / (y_fuel2 * 1e6)
+
+```
+"""
+
+# ╔═╡ 6315913c-d21d-4215-b38e-c02c5ba4bb18
+md"""
+We recover the adiabatic flame temperature as reported by [Law et al. (2006)](https://doi.org/10.1016/j.combustflame.2006.01.009) for reference.
+
+```csv
+0.4782270606531882,1600.0000000000000
+0.5248833592534992,1702.5104602510460
+0.5870917573872473,1809.2050209205020
+0.6710730948678072,1959.8326359832636
+0.7395023328149300,2074.8953974895394
+0.7954898911353032,2164.8535564853555
+0.8576982892690513,2248.5355648535565
+0.9323483670295489,2330.1255230125520
+0.9883359253499223,2371.9665271966530
+1.0474339035769828,2392.8870292887027
+1.1189735614307930,2390.7949790794980
+1.1998444790046658,2365.6903765690377
+1.2838258164852254,2330.1255230125520
+1.3833592534992225,2290.3765690376567
+1.4673405909797825,2256.9037656903765
+1.5544323483670293,2223.4309623430963
+1.6508553654743392,2185.7740585774060
+1.7255054432348370,2156.4853556485355
+1.8841368584758942,2097.9079497907950
+2.0427682737169520,2043.5146443514643
+2.2013996889580090,1993.3054393305438
+2.3351477449455675,1949.3723849372384
+2.3942457231726280,1928.4518828451883
+```
+
+```python
+df = pd.read_csv("media/adiabatic_flame_temperature.csv",
+                 header=None, names=["phi", "T"])
+phi_ref, T_ref = df[["phi", "T"]].to_numpy().T
+
+plt.close("all")
+plt.style.use("seaborn-white")
+fig, ax = plt.subplots(1, 3, figsize=(12, 4))
+
+ax[0].plot(phi, T_complete, label="Complete")
+ax[0].plot(phi, T_incomplete, label="Incomplete")
+ax[0].plot(phi_ref, T_ref, "+", label="Law et al. (2006)")
+ax[0].grid(linestyle=":")
+ax[0].set_xlabel("Equivalence ratio [-]")
+ax[0].set_ylabel("Temperature [K]")
+ax[0].set_xlim(0.5, 2.5)
+ax[0].set_ylim(1600, 2500)
+ax[0].legend()
+
+ax[1].plot(phi, X_complete, label="Complete")
+ax[1].plot(phi, X_incomplete, label="Incomplete")
+ax[1].grid(linestyle=":")
+ax[1].set_xlabel("Equivalence ratio [-]")
+ax[1].set_ylabel("Mole fraction of $\mathrm{O_2}$")
+ax[1].set_xlim(0.5, 2.5)
+ax[1].set_ylim(0.0, 0.1)
+ax[1].legend()
+
+ax[2].plot(phi, lhv_complete, label="Complete")
+ax[2].plot(phi, lhv_incomplete, label="Incomplete")
+ax[2].grid(linestyle=":")
+ax[2].set_xlabel("Equivalence ratio [-]")
+ax[2].set_ylabel("LHV $\mathrm{MJ\cdotp{}kg^{-1}}$")
+ax[2].set_xlim(0.5, 2.5)
+ax[2].set_ylim(40.0, 130.0)
+ax[2].legend()
+
+fig.tight_layout()
+```
+"""
+
+# ╔═╡ dd21fb11-59e6-4c0d-9218-7a5b2a48173f
+md"""
+```python
+def ignition_delay(states, species):
+    "Ignition delay from peak in species concentration. "
+    return states.t[states(species).Y.argmax()]
+
+def make_simulation(gas, max_time_step=0.001, atol=1.0e-15, rtol=1.0e-10):
+    "Create reactor for ignition time simulation."
+    reactor = ct.IdealGasConstPressureReactor(gas)
+    reactor.volume = 1.0
+    reactor.chemistry_enabled = True
+    reactor.energy_enabled = True
+
+    network = ct.ReactorNet([reactor])
+    network.max_time_step = max_time_step
+    network.atol = atol
+    network.rtol = rtol
+
+    return network, reactor
+
+reference_species = "OH"
+estimated_ignition_delay_time = 0.0002
+phi = 1.0
+
+gas = ct.Solution("gri30.yaml")
+gas.TP = 1200.0, ct.one_atm
+gas.set_equivalence_ratio(phi, X_fuel, X_oxid, basis="mole")
+
+network, reactor = make_simulation(gas)
+time_history = ct.SolutionArray(gas, extra="t")
+
+t = 0
+time_history.append(reactor.thermo.state, t=t)
+
+while t < estimated_ignition_delay_time:
+    t = network.step()
+    time_history.append(reactor.thermo.state, t=t)
+
+tau = ignition_delay(time_history, reference_species) * 1e6
+```
+
+We recover the ignition time as reported by [Zhao et al. (2011)](https://doi.org/10.1007/s11434-010-4345-3) for reference.
+
+The next plot illustrates good agreement even though simulation was not carried with same mechanism.
+"""
+
+# ╔═╡ a8e96db8-61e4-4a63-b57b-94a380ae6682
+md"""
+```csv
+t,T
+0.0000000000000000,1203.4782608695655
+28.971193415637870,1196.5217391304348
+36.543209876543216,1206.9565217391305
+41.481481481481495,1297.3913043478262
+43.950617283950635,1457.3913043478262
+46.090534979423880,1673.0434782608695
+48.230452674897140,1822.6086956521738
+52.345679012345684,2013.9130434782610
+57.448559670781904,2170.4347826086955
+64.691358024691370,2313.0434782608695
+71.769547325102880,2400.0000000000000
+78.683127572016470,2469.5652173913045
+85.925925925925950,2521.7391304347825
+93.333333333333370,2566.9565217391305
+101.06995884773664,2601.7391304347825
+108.47736625514406,2629.5652173913045
+115.39094650205763,2650.4347826086955
+120.00000000000000,2660.8695652173915
+```
+
+```csv
+t,Y
+0.16415868673050937,0.00000000000000000
+29.0560875512995840,0.00000000000000000
+37.0998632010943940,0.25684931506849296
+41.6963064295485700,2.73972602739726060
+43.6662106703146400,6.93493150684931700
+45.1436388508891900,11.7294520547945210
+47.4418604651162800,16.4383561643835630
+51.2175102599179300,20.5479452054794500
+56.1422708618331100,22.6027397260274000
+64.3502051983584100,23.6301369863013730
+75.5129958960328300,23.8013698630137000
+87.9890560875512900,23.2876712328767100
+101.778385772913820,22.6883561643835630
+118.850889192886460,22.0034246575342460
+```
+
+```python
+t = time_history.t * 1e6
+T = time_history.T
+Y = time_history(reference_species).Y * 1e3
+
+df = pd.read_csv("media/hydrogen_ignition_delay_Y.csv",
+                 header=None, names=["t", "Y"])
+t_ref_Y, Y_ref = df[["t", "Y"]].to_numpy().T
+
+df = pd.read_csv("media/hydrogen_ignition_delay_T.csv",
+                 header=None, names=["t", "T"])
+t_ref_T, T_ref = df[["t", "T"]].to_numpy().T
+
+plt.close("all")
+plt.style.use("seaborn-white")
+fig, ax = plt.subplots(1, 2, figsize=(10, 4))
+fig.suptitle(f"Computed Ignition Delay: {tau:.3f} ms")
+
+ax[0].plot(t, Y, label="Computed Gri-Mech 3.0")
+ax[0].plot(t_ref_Y, Y_ref, "+", label="Zhao et al. (2011)")
+ax[0].grid(linestyle=":")
+ax[0].set_xlabel("Time [$\mu$s]")
+ax[0].set_ylabel("Mole fraction of $\mathrm{OH}$")
+ax[0].set_xlim(0.0, 120.0)
+ax[0].set_ylim(0.0, 30.0)
+ax[0].legend(loc=2)
+
+ax[0].axvline(tau, linestyle=":", color="k")
+ax[0].arrow(0.0, 15.0, tau, 0.0, width=0.001,
+            head_width=0.5, head_length=3.0,
+            length_includes_head=True, color="r",
+            shape="full")
+ax[0].annotate(r"$Ignition Delay: \tau_{ign}$", xy=(0, 0), 
+               xytext=(2, 16), fontsize=12)
+
+ax[1].plot(t, T, label="Computed Gri-Mech 3.0")
+ax[1].plot(t_ref_T, T_ref, "+", label="Zhao et al. (2011)")
+ax[1].grid(linestyle=":")
+ax[1].set_xlabel("Time [$\mu$s]")
+ax[1].set_ylabel("Temperature [K]")
+ax[1].set_xlim(0.0, 120.0)
+ax[1].set_ylim(1000.0, 2800.0)
+ax[1].legend(loc=2)
+
+fig.tight_layout()
+```
+"""
+
+# ╔═╡ 6545b4fe-051a-4956-ba9c-7f99567968a0
+md"""
+Another important element to show is the negative temperature coefficient (NTC) of the mechanism.
+
+This is shown by demonstrating a faster combustion with increased initial temperature.
+"""
+
+# ╔═╡ f4e2edaf-0e63-45ad-bd62-e05f34632f54
+md"""
+```python
+T = np.arange(3000, 999, -25)
+
+estimated_ignition_delay_times = 0.002 * np.ones_like(T, dtype=float)
+
+extra = {"tau": estimated_ignition_delay_times}
+ignition_delays = ct.SolutionArray(gas, shape=T.shape, extra=extra)
+ignition_delays.set_equivalence_ratio(phi, X_fuel, X_oxid, basis="mole")
+ignition_delays.TP = T, ct.one_atm
+
+
+for k, state in enumerate(ignition_delays):
+    gas.TPX = state.TPX
+    network, reactor = make_simulation(gas)
+    reference_species_history = []
+    time_history = []
+
+    t = 0
+    while t < estimated_ignition_delay_times[k]:
+        t = network.step()
+        time_history.append(t)
+        reference_species_history.append(gas[reference_species].X[0])
+
+    i_ign = np.array(reference_species_history).argmax()
+    ignition_delays.tau[k] = time_history[i_ign] * 1e6
+
+plt.close("all")
+plt.style.use("seaborn-white")
+fig, ax = plt.subplots(1, 1, figsize=(6, 4))
+fig.suptitle(f"Negative Temperature Coefficient Check")
+ax.plot(1000 / ignition_delays.T, ignition_delays.tau)
+ax.grid(linestyle=":")
+ax.set_ylabel("Ignition Delay [$\mu$s]")
+ax.set_xlabel("$1000\\times{}T^{-1}\:[K^{-1}]$ ")
+ax.set_xlim(0.3, 1.0)
+ax.set_ylim(45.0, 355.0)
+fig.tight_layout()
+```
+"""
 
 # ╔═╡ Cell order:
 # ╟─9d15ec6c-04a7-4f5c-908b-310a774ff044
@@ -790,4 +1114,9 @@ md"""
 # ╟─215e07f8-faca-49f7-a0b5-b5f53b844b80
 # ╟─953234a8-cc26-4606-bb97-ebc23e1c7322
 # ╟─ff5573c6-2a94-455a-8d7f-f8ff2ab384e7
-# ╠═37c27593-b099-417f-83c4-517eeb670aba
+# ╟─37c27593-b099-417f-83c4-517eeb670aba
+# ╟─6315913c-d21d-4215-b38e-c02c5ba4bb18
+# ╟─dd21fb11-59e6-4c0d-9218-7a5b2a48173f
+# ╟─a8e96db8-61e4-4a63-b57b-94a380ae6682
+# ╟─6545b4fe-051a-4956-ba9c-7f99567968a0
+# ╟─f4e2edaf-0e63-45ad-bd62-e05f34632f54
