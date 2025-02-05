c07fd4608d8e6f92cd032cd80a9fc85b271fde05
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2025-02-05 09:19:13 +0100

    Stripping

--- README.md
+++
@@ -1,5 +0,0 @@
-# WallyToolbox.jl
-
-General tools I use everyday for everything.
-
-For more, please read the [docs](https://wallytutor.github.io/WallyToolbox.jl/dev/).
--- src/py/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-from .version import author as __author__
-from .version import version as __version__
-
-__all__ = ["__author__", "__version__"]
