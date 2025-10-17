ddd1a58688bc3eb91e1778719ebc58ff033f6ff7
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2025-10-17 18:05:29 +0200

    Flatten

--- tools/bin/code.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-call %~dp0source.bat
-Code.exe --extensions-dir=%VSCODEEXTENSIONS% --user-data-dir=%VSCODESETTINGS% -r %HERE%../../../WallyToolbox.jl
--- tools/bin/wjulia.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-julia --project=%~dp0../.. -i -e "using WallyToolbox" %*
